8d8f69c6ae4ad24d31663ab560e7985bde147b67
Kavee-ginty <kaveeshaginodh1@gmail.com>
2026-04-13 19:17:50 +0530

    both devices send data

---
+++ .pio/build/camdock_s3/FrameworkArduino/firmware_msc_fat.c.d
@@ -0,0 +1,3 @@
+.pio/build/camdock_s3/FrameworkArduino/firmware_msc_fat.c.o: \
+ C:/Users/Kaveesha\ Ginodh/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
+ C:/Users/Kaveesha\ Ginodh/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
